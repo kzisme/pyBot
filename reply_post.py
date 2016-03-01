6c278ba817c410d34628bfe607cab15a75086460
Kevin Zita <bleedgreenandgold@gmail.com>
2016-03-01 15:45:12 -0500

    Bot correctly comments to new posts and logs the corresponding id's of posts it replied to

--- reply_post.py
+++ reply_post.py
@@ -11,7 +11,7 @@ if not os.path.isfile("config_bot.py"):
 user_agent = ("PyFor Eng bot 0.1")
 r = praw.Reddit(user_agent = user_agent)
 
-r.login(REDDIT_USERNAME, REDDIT_PASS)
+r.login(REDDIT_USERNAME, REDDIT_PASS, disable_warning = True)
 
 if not os.path.isfile("posts_replied_to.txt"):
     posts_replied_to = []
@@ -21,21 +21,20 @@ else:
         posts_replied_to = posts_replied_to.split("\n")
         posts_replied_to = filter(None, posts_replied_to)
 
-subreddit = r.get_subreddit('pythonforengineers')
-for submission in subreddit.get_hot(limit=5):
+subreddit = r.get_subreddit('EDC')
+for submission in subreddit.get_new(limit = 25):
 
     if submission.id not in posts_replied_to:
 
-        # Do a case insensitive search
-        if re.search("i love python", submission.title, re.IGNORECASE):
-            # Reply to the post
-            submission.add_comment("Nigerian scammer bot says: It's all about the Bass (and Python)")
-            print "Bot replying to : ", submission.title
+        submission.add_comment("""I see you have made a new post!
+        Please make sure you have added a list of items pictured""")
 
-            # Store the current id into our list
-            posts_replied_to.append(submission.id)
+        posts_replied_to.append(submission.id)
+        print "Bot replying to : ", submission.title
 
-
-with open("posts_replied_to.txt", "w") as f:
+        # Store the current id into our list
+        posts_replied_to.append(submission.id)
+# Using "a" to append to .txt as opposed to "w" write
+with open("posts_replied_to.txt", "a") as f:
     for post_id in posts_replied_to:
         f.write(post_id + "\n")
